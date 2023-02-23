335f8500ab5951ecf2a37a8056a0c5a4c1fe9e70
salvadorceniceros <salvadorceniceros@uadec.edu.mx>
2023-02-23 00:09:49 -0600

    agregando mi CV al proyecto, el logo y favicon

--- script.js
+++ script.js
@@ -16,3 +16,29 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("html");
+        habilidades[2].classList.add("css");
+        habilidades[3].classList.add("photoshop");
+        habilidades[4].classList.add("joomla");
+        habilidades[5].classList.add("wordpress");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajoEquipo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("proyectos");
+        habilidades[11].classList.add("flexibilidad");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
